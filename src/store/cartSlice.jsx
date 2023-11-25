937506757f99cad189f1fd8b89c29d1425622d47
Mohit <109670799+Mohit-au50@users.noreply.github.com>
2023-11-25 15:17:58 +0530

    new

--- src/store/cartSlice.jsx
+++ src/store/cartSlice.jsx
@@ -7,66 +7,17 @@ const initialState = {
 };
 
 const cartSlice = createSlice({
-  name: "cart",
+  name: "userBooking",
   initialState,
   reducers: {
-    add: (state, action) => {
-      const productId = state.cartItems.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      if (productId >= 0) {
-        state.cartItems[productId].quantity += 1;
-      } else {
-        const productQuantity = { ...action.payload, quantity: 1 };
-        state.cartItems.push(productQuantity);
-      }
-    },
-
-    remove: (state, action) => {
-      const productId = state.cartItems.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      if (state.cartItems[productId].quantity > 1) {
-        state.cartItems[productId].quantity -= 1;
-      } else if (state.cartItems[productId].quantity === 1) {
-        const cartProducts = state.cartItems.filter(
-          (item) => item.id !== action.payload.id
-        );
-        state.cartItems = cartProducts;
-      }
-    },
-
-    removeAll: (state, action) => {
-      const cartProducts = state.cartItems.filter(
-        (item) => item.id !== action.payload.id
-      );
-      state.cartItems = cartProducts;
-    },
-
-    calculateTotal: (state, action) => {
-      let { amount, quantity } = state.cartItems.reduce(
-        (subTotal, cartItems) => {
-          const { price, quantity } = cartItems;
-          const itemTotal = price * quantity;
-
-          subTotal.amount += itemTotal;
-          subTotal.quantity += quantity;
-
-          return subTotal;
-        },
-        {
-          amount: 0,
-          quantity: 0,
-        }
-      );
-      amount = parseFloat(amount.toFixed(2));
-      state.totalAmount = amount;
-      state.totalQuantity = quantity;
-    },
+    userCurrentbooking: (state, action) => {},
+    userTotalbooking: (state, action) => {},
+    addHotel: (state, action) => {},
+    removeHotel: (state, action) => {},
+    calculateTotal: (state, action) => {},
   },
 });
 
-export const { add, remove, removeAll, calculateTotal } = cartSlice.actions;
+export const { userbooking, remove, removeAll, calculateTotal } =
+  cartSlice.actions;
 export default cartSlice.reducer;
